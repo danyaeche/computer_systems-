ca27528e8d48a0c171a75b5d648601f2076ab00e
danyaeche <davidio7943@gmail.com>
2020-01-29 22:07:26 -0800

    2.58c

--- ch_1/excercises/2.58.c
+++ ch_1/excercises/2.58.c
@@ -1,4 +1,31 @@
-
+/*
+ * Write a procedure is_little_endian that will return 1 when compiled and run on a little-endian machine, and will return 0
+ * when compiled and run on a big- endian machine. This program should run on any machine, regardless of its word size.
+ */
 
 
 #include <stdio.h>
+
+int is_little_endian(void){
+    int x = 0;
+
+    /* MSB = 0, LSB = 1
+     * if Little Endian: MSB = 1, LSB = 0
+     */
+
+    return x | 1;
+
+};
+
+
+main(){
+
+    if (is_little_endian() == 0) {
+        printf("This machine is big endian");
+    } else {
+        printf("This machine is little endian");
+    }
+
+    return 0;
+}
+
---
+++ ch_1/excercises/2.59.c
@@ -0,0 +1,16 @@
+/*
+ *Write a C expression that will yield a word consisting of the least significant byte of x
+ * and the remaining bytes of y.
+ * For operands x = 0x89ABCDEF and y = 0x76543210, this would give 0x765432EF.
+ */
+
+
+#include <stdio.h>
+define unsigned char *byte_pointer
+
+
+
+
+
+
+main()
